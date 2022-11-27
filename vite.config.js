22b5610aec3289c44f24200b94c28f9125f72cfb
Kamil Michalski <kmichalski@outlook.com>
2022-11-27 16:27:39 +0100

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        login: resolve(__dirname, "edit.html"),
-        register: resolve(__dirname, "add.html"),
-        register: resolve(__dirname, "auth", "login.html"),
+        edit: resolve(__dirname, "edit.html"),
+        add: resolve(__dirname, "add.html"),
+        login: resolve(__dirname, "auth", "login.html"),
         register: resolve(__dirname, "auth", "register.html"),
-        register: resolve(__dirname, "auth", "profile.html"),
+        profile: resolve(__dirname, "auth", "profile.html"),
       },
     },
   },
